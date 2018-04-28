209d41f38fe9cd39c99df40566f138e21d2e21a1
Jawa89 <camil.champin@gmail.com>
2018-04-28 16:29:54 +0200

    logic games

--- other/js/main.js
+++ other/js/main.js
@@ -2,7 +2,6 @@ window.onload=function() {
   canvas = document.getElementById("bc");
   ctx = canvas.getContext("2d");
   document.addEventListener("keydown", KeyPressed)
-  console.log(ctx);
   setInterval(update, 1000 / 15);
 }
 
@@ -12,8 +11,15 @@ ay = Math.floor(Math.random() * 49);
 snakeLength = 20;
 vx = 0;
 vy = 0;
-snakeWidth = 20;
-pts = 0
+trail = [];
+tail = 3;
+
+function draw_canvas() {
+  ctx.fillStyle = "#545555";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#9966ff"
+  ctx.fillRect(ax*20,ay*20,20,20)
+}
 
 function KeyPressed(evt)
 {
@@ -26,46 +32,49 @@ function KeyPressed(evt)
       vx = 0;
       vy = -1;
       break;
-      case 39:
-        vx = 1;
-        vy = 0;
-        break;
-      case 37:
-        vx = -1;
-        vy = 0;
-        break;
+    case 39:
+      vx = 1;
+      vy = 0;
+      break;
+    case 37:
+      vx = -1;
+      vy = 0;
+      break;
   }
-
 }
 
-function draw_canvas() {
-  ctx.fillStyle = "#545555";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#ff0000";
-  ctx.fillRect(px * 20, py * 20, snakeLength, snakeWidth);
-  ctx.fillStyle = "#33cc33";
-  ctx.fillRect(ax * 20,ay * 20,20,20);
-}
+var score = document.getElementById("sc");
 
 function update() {
   draw_canvas();
 
-  if (px == ax && py == ay) {
-    ax = Math.floor(Math.random() * 49);
-    ay = Math.floor(Math.random() * 49);
-    snakeLength += 10;
-    pts += 100
-  }
-  if (px>49 || py>49 || px<1 || py<1) {
-    px = py = 25;
-    vx = vy = 0;
-    snakeLength = snakeWidth = 20;
-    ax = Math.floor(Math.random() * 49);
-    ay = Math.floor(Math.random() * 49);
-
-  }
+  score.innerHTML = sc;
 
   px += vx;
   py += vy;
-  document.getElementById("title").write("test");
+  console.log(vx);
+  console.log(vy);
+  ctx.fillStyle = "#33cc33";
+  for (var i = 0; i < trail.length; i++) {
+      ctx.fillRect(trail[i].x * 20, trail[i].y * 20, 18, 18);
+      if (trail[i].x == px && trail[i].y == py)
+        tail = 3;
+  }
+  trail.push({x: px, y: py});
+  while (trail.length > tail)
+    trail.shift();
+  if (px==ax && py==ay) {
+    tail+=1;
+    sc += 5;
+    ax = Math.floor(Math.random() * 49);
+    ay = Math.floor(Math.random() * 49);
+  }
+  if (px>49 || py>49 || px<1 || py<1) {
+    tail = 3;
+    px = py = 25;
+    ax = Math.floor(Math.random() * 49);
+    ay = Math.floor(Math.random() * 49);
+    vx = 0;
+    vy = 0;
+  }
 }
